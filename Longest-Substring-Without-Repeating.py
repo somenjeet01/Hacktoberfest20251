df42cea6e0360ebfeb2699aaf3eafd3fccf34e92
Somen Jeet <somenjeet2004@gmail.com>
2025-11-08 13:27:10 +0530

    Implement function to find longest substring length

---
+++ Longest-Substring-Without-Repeating.py
@@ -0,0 +1,9 @@
+def lengthOfLongestSubstring(s):
+    seen = {}
+    left = max_len = 0
+    for right, ch in enumerate(s):
+        if ch in seen and seen[ch] >= left:
+            left = seen[ch] + 1
+        seen[ch] = right
+        max_len = max(max_len, right - left + 1)
+    return max_len
